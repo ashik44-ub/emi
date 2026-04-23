e2d1cd6554285f4e67ad9e93a60d7401c88c2038
ashik44-ub <shopnilashik4@gmail.com>
2026-04-23 20:34:06 +0600

    update to all

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://emi-backend-sys.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
